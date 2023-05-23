4a0efe0876aaf9cfd3b63c2782556bde687e8eb5
palace22 <palazzolo1995@gmail.com>
2023-05-23 19:10:53 +0300

    update task starter

--- contracts/nft_as_collateral.py
+++ contracts/nft_as_collateral.py
@@ -3,5 +3,53 @@ from beaker import *
 from contracts.helpers.checks import *
 from contracts.helpers.inners import *
 
-nft_as_collateral_app = Application("NFT Lending")
 
+class State:
+    collateral_id = LocalStateValue(stack_type=TealType.uint64)
+    borrowing_id = LocalStateValue(stack_type=TealType.uint64)
+    amount = LocalStateValue(stack_type=TealType.uint64)
+    duration = LocalStateValue(stack_type=TealType.uint64)
+    interest = LocalStateValue(stack_type=TealType.uint64)  # 2 d.p.
+
+    start = LocalStateValue(stack_type=TealType.uint64)
+    lender = LocalStateValue(stack_type=TealType.bytes)
+
+
+nft_as_collateral_app = Application("NFTasCollateral", state=State)
+
+
+@nft_as_collateral_app.external
+def opt_in_nft():
+    return Approve()
+
+
+@nft_as_collateral_app.opt_in(bare=True)
+def opt_in_borrower():
+    return Approve()
+
+
+# Borrower
+@nft_as_collateral_app.external
+def request_loan():
+    return Approve()
+
+
+@nft_as_collateral_app.external
+def delete_request():
+    return Approve()
+
+
+@nft_as_collateral_app.external
+def repay_loan():
+    return Approve()
+
+
+# Lender
+@nft_as_collateral_app.external
+def accept_loan():
+    return Approve()
+
+
+@nft_as_collateral_app.external
+def liquidate_loan():
+    return Approve()
